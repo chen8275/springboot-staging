f8c8770bd90b0d819dbfccf08ae6486ba4f1d42e
chendesheng <chendesheng@tuhu.cn>
2019-05-06 15:55:16 +0800

    集成generator自动生成model，xml，dao功能

---
+++ src/main/java/com/example/demo/core/constant/ProjectConstant.java
@@ -0,0 +1,37 @@
+ /*
+  * Copyright 2019 tuhu.cn All right reserved. This software is the
+  * confidential and proprietary information of tuhu.cn ("Confidential
+  * Information"). You shall not disclose such Confidential Information and shall
+  * use it only in accordance with the terms of the license agreement you entered
+  * into with Tuhu.cn
+  */
+ package com.example.demo.core.constant;
+
+ /**
+  * @author chendesheng chendesheng@tuhu.cn
+  * @since 2019/5/6 15:44
+  */
+ public class ProjectConstant {
+     
+     // 项目基础包名称
+     public static final String BASE_PACKAGE = "com.example.demo";
+    
+     // Model所在包
+     public static final String MODEL_PACKAGE = BASE_PACKAGE + ".model";
+    
+     // Mapper所在包
+     public static final String MAPPER_PACKAGE = BASE_PACKAGE + ".dao";
+    
+     // Service所在包
+     public static final String SERVICE_PACKAGE = BASE_PACKAGE + ".service";
+    
+     // ServiceImpl所在包
+     public static final String SERVICE_IMPL_PACKAGE = SERVICE_PACKAGE + ".impl";
+    
+     // Controller所在包
+     public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".controller";
+    
+     // Mapper插件基础接口的完全限定名
+     public static final String MAPPER_INTERFACE_REFERENCE = BASE_PACKAGE + ".core.universal.Mapper";
+     
+ }
--- src/main/java/com/example/demo/dao/UserInfoMapper.java
+++ src/main/java/com/example/demo/dao/UserInfoMapper.java
@@ -7,25 +7,5 @@ import java.util.List;
 
 public interface UserInfoMapper extends Mapper<UserInfo> {
     
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(UserInfo record);
-
-    int insertSelective(UserInfo record);
     
-    /**
-     * 通过id查找
-     * @param id
-     * @return
-     */
-    UserInfo selectByPrimaryKey(Integer id);
-    
-    /**
-     * 查询所有用户
-     * @return
-     */
-    List<UserInfo> selectAll();
-    int updateByPrimaryKeySelective(UserInfo record);
-
-    int updateByPrimaryKey(UserInfo record);
 }
